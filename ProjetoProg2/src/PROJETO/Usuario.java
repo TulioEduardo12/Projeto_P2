d56ad033562071c12c69988863bfb0411fcf2c09
Matheus Dino <matheusthiago.dino@gmail.com>
2023-08-25 00:59:26 -0300

    add getters e setters, alem de hashcode e equals, esse commit é mais pra ver se ta tudo certo e se ta indo pra branch de testes

--- ProjetoProg2/src/PROJETO/Usuario.java
+++ ProjetoProg2/src/PROJETO/Usuario.java
@@ -4,6 +4,8 @@
  */
 package PROJETO;
 
+import java.util.Objects;
+
 /**
  *
  * @author Tulio E
@@ -23,6 +25,68 @@ class Usuario {
         System.out.println(nome + " comprou um ingresso no valor de " + ingresso.getPreco());
         ingresso.mostrarDetalhes();
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nome=" + nome + ", cpf=" + cpf + ", email=" + email + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.nome);
+        hash = 41 * hash + Objects.hashCode(this.cpf);
+        hash = 41 * hash + Objects.hashCode(this.email);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.cpf, other.cpf)) {
+            return false;
+        }
+        return Objects.equals(this.email, other.email);
+    }
+    
+    
+    
 }
 
 
